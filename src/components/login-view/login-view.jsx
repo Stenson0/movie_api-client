d7891ec7bc94625f435ec5970b4c960a2e12457b
Stenson0 <161175213+Stenson0@users.noreply.github.com>
2025-03-03 17:45:25 -0600

    Added login-view

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,50 @@
+import {useState} from 'react';
+
+export const LoginView = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+          access: username,
+          secret: password,
+        };
+
+        fetch("https://mymovie-api-cc1cba8fc12b.herokuapp.com/login", {
+            method: "POST",
+            body: JSON.stringify(data),
+          }).then((response) => {
+            if (response.ok) {
+              onLoggedIn(username);
+            } else {
+              alert("Login failed");
+            }
+          });
+        };
+
+    return (
+    <form onSubmit={handleSubmit}>
+        <label>
+            Username:
+            <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            />
+        </label>
+        <label>
+            Password:
+            <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            />
+        </label>
+        <button type="submit">Submit</button>
+    </form>
+  );
+};
